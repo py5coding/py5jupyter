6d92b68c57f37a21a4a27ae424ca8197783475e4
Jim <james.schmitz@gmail.com>
2022-05-07 21:40:15 -0400

    use system time and fix click count

--- py5jupyter/widgets/sketchportal.py
+++ py5jupyter/widgets/sketchportal.py
@@ -42,14 +42,16 @@ class Py5SketchPortal(DOMWidget):
         from py5 import Py5MouseEvent, Py5KeyEvent
 
         event_type = content.get("event", "")
+        event_x = int(content["x"])
+        event_y = int(content["y"])
 
         if event_type == "mouse_enter":
-            self.sketch._instance.fakeMouseEvent(0, Py5MouseEvent.ENTER, 0, int(content["x"]), int(content["y"]), py5.LEFT, 0)
+            self.sketch._instance.fakeMouseEvent(Py5MouseEvent.ENTER, 0, event_x, event_y, py5.LEFT, 0)
         elif event_type == "mouse_down":
-            self.sketch._instance.fakeMouseEvent(0, Py5MouseEvent.PRESS, 0, int(content["x"]), int(content["y"]), py5.LEFT, 0)
+            self.sketch._instance.fakeMouseEvent(Py5MouseEvent.PRESS, 0, event_x, event_y, py5.LEFT, 1)
         elif event_type == "mouse_move":
-            self.sketch._instance.fakeMouseEvent(0, Py5MouseEvent.MOVE, 0, int(content["x"]), int(content["y"]), py5.LEFT, 0)
+            self.sketch._instance.fakeMouseEvent(Py5MouseEvent.MOVE, 0, event_x, event_y, py5.LEFT, 0)
         elif event_type == "mouse_up":
-            self.sketch._instance.fakeMouseEvent(0, Py5MouseEvent.RELEASE, 0, int(content["x"]), int(content["y"]), py5.LEFT, 0)
+            self.sketch._instance.fakeMouseEvent(Py5MouseEvent.RELEASE, 0, event_x, event_y, py5.LEFT, 1)
         elif event_type == "mouse_leave":
-            self.sketch._instance.fakeMouseEvent(0, Py5MouseEvent.EXIT, 0, int(content["x"]), int(content["y"]), py5.LEFT, 0)
+            self.sketch._instance.fakeMouseEvent(Py5MouseEvent.EXIT, 0, event_x, event_y, py5.LEFT, 0)
