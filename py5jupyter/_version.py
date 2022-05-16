75c582663c04a57e4426c16f9d4af2d651be0f37
Jim <james.schmitz@gmail.com>
2022-05-16 12:37:43 -0400

    bump version

--- py5jupyter/_version.py
+++ py5jupyter/_version.py
@@ -17,5 +17,5 @@
 #   along with this library. If not, see <https://www.gnu.org/licenses/>.
 #
 # *****************************************************************************
-version_info = (0, 1, 1, 'a0')
+version_info = (0, 1, 2, 'a0')
 __version__ = ".".join(map(str, version_info))
